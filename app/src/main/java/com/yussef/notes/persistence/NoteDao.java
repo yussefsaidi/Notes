f3c71b7d5cb005128d902833958c2d0a7682f0ad
Yussef <saidi.yussef@live.fr>
2020-01-27 16:28:41 -0500

    Add Note Data Access Object for Room

---
+++ app/src/main/java/com/yussef/notes/persistence/NoteDao.java
@@ -0,0 +1,26 @@
+package com.yussef.notes.persistence;
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import com.yussef.notes.models.Note;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    long[] insertNotes(Note... notes);
+
+    @Query("SELECT * FROM notes")
+    LiveData<List<Note>> getNotes();
+
+    @Delete
+    int delete(Note... notes);
+
+    @Update
+    int update(Note... notes);
+}
