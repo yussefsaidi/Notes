752f1d4cc4195a527a5f14a3db61f790ce55a1bb
Yussef <saidi.yussef@live.fr>
2020-01-28 11:57:27 -0500

    Add Room Repository

---
+++ app/src/main/java/com/yussef/notes/persistence/NoteRepository.java
@@ -0,0 +1,35 @@
+package com.yussef.notes.persistence;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import com.yussef.notes.models.Note;
+
+import java.util.List;
+
+public class NoteRepository {
+
+    private NoteDatabase mNoteDatabase;
+
+    public NoteRepository(Context context){
+        mNoteDatabase = NoteDatabase.getInstance(context);
+    }
+
+    public void insertNoteTask(Note note){
+
+    }
+
+    public void updateNote(Note note){
+
+    }
+
+    public LiveData<List<Note>> retrieveNotesTask(){
+        return mNoteDatabase.getNoteDao().getNotes();
+    }
+
+    public void deleteNote(Note note){
+
+    }
+
+}
